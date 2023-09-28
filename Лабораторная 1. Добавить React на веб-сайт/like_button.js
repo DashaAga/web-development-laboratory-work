c1dd14a75b6a8acbcff1f3d45c84d3ab2aa27271
Dasha Agapenkova <93427597+DashaAga@users.noreply.github.com>
2023-09-28 22:05:06 +0300

    New semester

---
+++ Лабораторная 1. Добавить React на веб-сайт/like_button.js
@@ -0,0 +1,30 @@
+'use strict';
+var commentid = 10;
+const e = React.createElement;
+class LikeButton extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = { liked: false };
+    }
+    render() {
+    	if (this.state.liked) {
+    		return 'You liked ' + this.props.commentid;
+        }
+        return e(
+        	'button',
+    	    { onClick: () => this.setState({ liked: true }) },
+           'Like'
+        );
+    }
+}
+
+const domContainer = document.querySelector('#like_button_container')
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+    	const commentID = parseInt(domContainer.dataset.commentid, 10);
+    	ReactDOM.render(
+    		e(LikeButton, { commentID: commentID}),
+    		domContainer
+    		);
+    });
+ReactDOM.render(e(LikeButton), domContainer);
